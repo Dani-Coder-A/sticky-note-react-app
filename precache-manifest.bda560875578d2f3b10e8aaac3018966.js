2b90bec959e72f940a830366e74187cc00bdb38d
Dani A <danielle.arlinghaus25@gmail.com>
2022-03-10 20:08:20 -0500

    Updates

---
+++ precache-manifest.bda560875578d2f3b10e8aaac3018966.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2de429fdc1338623ba74",
+    "url": "/sticky-note-react-app/static/css/main.61f56220.chunk.css"
+  },
+  {
+    "revision": "2de429fdc1338623ba74",
+    "url": "/sticky-note-react-app/static/js/main.b70de5f6.chunk.js"
+  },
+  {
+    "revision": "d3c1d9f9d4203a979099",
+    "url": "/sticky-note-react-app/static/js/runtime~main.a1e074f2.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/sticky-note-react-app/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "32e0f644ebbd77f6dcfe2db5b2125252",
+    "url": "/sticky-note-react-app/index.html"
+  }
+];
